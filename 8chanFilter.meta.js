427d31675817e82eb549533e9c5179506cfe68da
nokosage <nokosage@charter.net>
2015-01-12 22:56:02 -0500

    v0.2.1

--- 8chanFilter.meta.js
+++ 8chanFilter.meta.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         8chan Filter
-// @version      0.2.0
+// @version      0.2.1
 // @namespace    nokosage
 // @description  Regular expression, point-and-click filtering on 8chan.
 // @author       nokosage
-// @include      *://*8chan.co/*/*.html*
+// @include      *://*8ch*/*/*.html*
 // @run-at       document-start
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/nokosage/8chan-Filter/master/8chanFilter.meta.js
