de535f6284572ab7e248a0ead8163f115153323d
nokosage <nokosage@charter.net>
2015-01-12 22:54:23 -0500

    8ch.net new domain

--- 8chanFilter.user.js
+++ 8chanFilter.user.js
@@ -4,7 +4,7 @@
 // @namespace    nokosage
 // @description  Regular expression, point-and-click filtering on 8chan.
 // @author       nokosage
-// @include      *://*8chan.co/*/*.html*
+// @include      *://*8ch*/*/*.html*
 // @run-at       document-start
 // @grant        none
 // @updateURL    https://raw.githubusercontent.com/nokosage/8chan-Filter/master/8chanFilter.meta.js
